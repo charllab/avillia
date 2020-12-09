6f05f543910c573d91969a1930a6647db91fbd44
charllab <developer.charl@gmail.com>
2020-12-09 11:39:10 -0800

    typography for buttons and nav

--- src/themes/avillia/front-page.php
+++ src/themes/avillia/front-page.php
@@ -13,6 +13,10 @@
                 <p class="lead">This is a lead paragraph: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum aspernatur enim incidunt ullam similique consequuntur reiciendis tenetur molestias modi, fuga illum, minus at tempora eum.</p>
                 <p>This is a normal paragraph: Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Cupiditate repudiandae harum atque commodi, iste assumenda quod quas aliquam. Culpa, quam officiis veniam, magnam inventore repudiandae esse qui voluptas aspernatur, maxime sequi minus iure assumenda reiciendis possimus. Nemo nobis dolore molestias inventore temporibus? Nihil praesentium consectetur quaerat culpa sequi.</p>
                 <p class="small">This is a small paragraph: Lorem ipsum, dolor sit amet consectetur, adipisicing elit. Commodi, accusantium. Asperiores, obcaecati.</p>
+                <a href="#" class="btn btn-primary">Button Primary</a>
+                <a href="#" class="btn btn-secondary">Button Secondary</a>
+                <a href="#" class="btn btn-white">Button White</a>
+                <a href="#" class="btn btn-dark">Button Dark</a>
             </div>
         </div>
     </div>
