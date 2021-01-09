95387b2897704dae03e43438d71e2bf909b592cc
charllab <developer.charl@gmail.com>
2021-01-09 13:08:42 -0800

    added basic styling

--- src/themes/avillia/blog.php
+++ src/themes/avillia/blog.php
@@ -13,30 +13,62 @@ get_header(); ?>
 
             <div class="container">
 
-                <div class="row justify-content-center align-items-center mb-2 border border-danger">
+                <div class="row justify-content-start align-items-start mb-2 blog--listing--row__control--padding">
 
-                <?php
-                $limit = 6;
+                    <?php
+                    $limit = 2;
 
-                $temp = $wp_query;
-                $wp_query = null;
+                    $temp = $wp_query;
+                    $wp_query = null;
 
-                $wp_query = new WP_Query();
-                $wp_query->query('posts_per_page=' . $limit . '&paged=' . $paged);
+                    $wp_query = new WP_Query();
+                    $wp_query->query('posts_per_page=' . $limit . '&paged=' . $paged);
 
-                while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-
-
+                    while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
 
 
-                        <div class="col-lg-4">
+                        <div class="col-lg-6 col-xxl-4">
 
-                            <div class="card">
-                                <div class="card-body">
-                                    <h2 class="h4 font-pop mb-35 text-info"><?php the_title(); ?></h2>
+                            <div class="card blog--listing__card rounded-0 border-0">
+
+                                <?php if (has_post_thumbnail()) : ?>
+                                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"
+                                       class="border-o">
+                                        <div
+                                            style="background-image: url(<?php the_post_thumbnail_url(); ?>);"
+                                            class="blog--listing__thumbnail d-block">
+                                        </div>
+                                    </a>
+                                <?php else : ?>
+                                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"
+                                       class="border-o">
+                                        <div
+                                            style="background-image: url(<?php bloginfo('template_url'); ?>/images/logo.svg);"
+                                            class="blog--listing__thumbnail blog--listing__thumbnail--fallback d-block">
+                                        </div>
+                                    </a>
+                                <?php endif; ?>
+
+                                <div class="card-body border-0">
+
+                                    <div class="row mx-0 mb-1">
+                                        <div class="col-md-6 px-0">
+
+                                            <?php $category = get_the_category();
+                                            echo $category[0]->cat_name;
+                                            ?>
+
+                                        </div><!-- col -->
+                                        <div class="col-md-6 text-md-right px-0">
+                                            <?php echo get_the_date('jS F, Y'); ?>
+                                        </div><!-- col -->
+                                    </div><!-- row-->
+
+
+                                    <h2 class="blog--listing__title font-pop mb-35 text-info"><?php the_title(); ?></h2>
                                     <p class="card-text"><?php echo get_excerpt(); ?></p>
                                 </div><!-- card-body -->
-                                <div class="card-footer">
+                                <div class="card-footer border-0">
                                     <a href="<?php the_permalink(); ?>"
                                        class="mt-auto">Read More</a>
                                 </div><!-- card-footer -->
@@ -45,7 +77,7 @@ get_header(); ?>
                         </div><!-- col-->
 
 
-                <?php endwhile; ?>
+                    <?php endwhile; ?>
 
                 </div><!-- row -->
 
